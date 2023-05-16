5e96ace138a1d143109d4760ab4bc6051aa71bb1
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-05-16 04:58:53 -0700

    file upload on test folder

---
+++ test1/test2/main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+extern char **environ;
+
+char **_tokenize(char *str);
+void get_env(char **env);
+int executor(char *command, char **tokens);
+void exit_child(char *tokens[], char *line);
+void display(void);
+char *path(char *tokens[]);
+int _exit_status(int status);
+
+#endif
